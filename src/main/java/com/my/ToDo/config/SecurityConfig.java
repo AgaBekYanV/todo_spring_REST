3c369d77fcdf682b4472b25234b6c994b92c89e5
Vyacheslav Agabekyan <agabekyanv@yahoo.com>
2025-07-06 19:38:55 +0300

    Add Spring Security with Basic Authentication

---
+++ src/main/java/com/my/ToDo/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.my.ToDo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Отключаем CSRF для простоты (для REST API это безопасно)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll() // Разрешаем доступ к Swagger без авторизации
+                        .anyRequest().authenticated() // Все остальные запросы требуют авторизации
+                )
+                .httpBasic(Customizer.withDefaults()); // Используем базовую HTTP-авторизацию
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("admin")
+                .password("{noop}password") // {noop} означает, что пароль не шифруется (для простоты)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
